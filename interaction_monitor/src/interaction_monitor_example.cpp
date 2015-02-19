114b431c78b6bd9528542992892431dc85500e9c
Felip Marti <felip.marti.carrillo@gmail.com>
2015-02-19 16:55:00 +0000

    [interaction_monitor] Extracting parser to another node (rosservice). Adding example node and publisher node (TODO)

--- interaction_monitor/src/interaction_monitor_example.cpp
+++ interaction_monitor/src/interaction_monitor_example.cpp
@@ -11,12 +11,10 @@ int main(int argc, char **argv)
     }
 
     ros::NodeHandle n;
-    ros::Rate loop_rate(1000);
-
     ros::ServiceClient client = n.serviceClient<data_parser::Parse>("parse_data");
     data_parser::Parse srv;
-
     srv.request.path = argv[1];
+
     if (client.call(srv)) {
         ROS_INFO("[interaction_monitor]: Calling parse_data_srv");
     }
@@ -24,36 +22,6 @@ int main(int argc, char **argv)
         ROS_ERROR("[interaction_monitor]: Failed to call service parse_data_srv");
         return 1;
     }
-
-
-    ROS_INFO("[interaction_monitor]: Start Publishing!");
-    int current_time = 0;
-
-    /// Main Loop
-    while (ros::ok()) {
-/*TODO
-        // Topic message
-        data_parser::DataParsed pub_msg[0];
-        std::vector<data_parser::AnnotationList> v =srv.response.data;
-
-
-        /// Filling message topics
-        for (int i=0; i<srv.response.data.size(); i++) {
-        
-        }
-        
-
-        /// Publishing messages
-
-*/
-        ros::spinOnce();
-
-        loop_rate.sleep();
-        ++current_time;
-    }
-
-
-
 
     return 0;
 }
